a5291b1373f44c340332aeb41359346204fed325
m-attallah <100219531+MohamedAtallaa@users.noreply.github.com>
2023-03-29 10:18:35 +0200

    Set up Photos Collection UI

---
+++ Starter Project/PhotosVC.swift
@@ -0,0 +1,18 @@
+//
+//  PhotosVC.swift
+//  Starter Project
+//
+//  Created by Mohamed Atallah on 29/03/2023.
+//
+
+import UIKit
+
+
+class PhotosVC: UIViewController {
+    
+    // MARK: - IBOutlets
+    @IBOutlet private weak var collectionView: UICollectionView!
+    @IBOutlet private weak var indicator: UIActivityIndicatorView!
+    
+    
+}
