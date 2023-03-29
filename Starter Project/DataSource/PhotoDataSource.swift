31d0e04ecfc38fd76b8f9f5052055359f32ae7a1
m-attallah <100219531+MohamedAtallaa@users.noreply.github.com>
2023-03-29 12:56:13 +0200

    configure PhotoDataSource class

---
+++ Starter Project/DataSource/PhotoDataSource.swift
@@ -0,0 +1,26 @@
+//
+//  PhotoDataSource.swift
+//  Starter Project
+//
+//  Created by Mohamed Atallah on 29/03/2023.
+//
+
+import UIKit
+
+
+class PhotoDataSource: NSObject, UICollectionViewDataSource {
+    
+    var photos = [Photo]()
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return photos.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PhotoCell", for: indexPath) as! PhotoCell
+        let photo = photos[indexPath.row]
+        cell.set(for: photo)
+        return cell
+    }
+    
+}
--- Starter Project/Views/Cells/FavoriteCell.swift
+++ Starter Project/Views/Cells/FavoriteCell.swift
@@ -33,6 +33,6 @@ class FavoriteCell: UITableViewCell {
             }
         }
     }
-    
+
 }
 
