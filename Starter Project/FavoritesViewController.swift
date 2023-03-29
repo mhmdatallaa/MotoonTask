2c172427a52ccec841c26ae960d41f99dd27dca3
m-attallah <100219531+MohamedAtallaa@users.noreply.github.com>
2023-03-29 10:51:35 +0200

    set up favorites UI

---
+++ Starter Project/FavoritesViewController.swift
@@ -0,0 +1,36 @@
+//
+//  FavoritesViewController.swift
+//  Starter Project
+//
+//  Created by Mohamed Atallah on 29/03/2023.
+//
+
+import UIKit
+
+
+
+class FavoritesViewController: UIViewController {
+    
+    // MARK: - IBOutlets
+    @IBOutlet private weak var tableView: UITableView!
+    @IBOutlet private weak var emptyState: UILabel!
+    
+    // MARK: - Properties
+
+    
+    
+    // MARK: - Life cycle
+    
+    
+    
+    // MARK: - Actions
+
+
+    
+}
+
+
+
+// MARK: - Extensions
+
+
